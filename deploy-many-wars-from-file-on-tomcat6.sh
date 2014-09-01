e254e88e274543c1e345f9155e7649b0b22ce6eb
Irina <iriiiina@gmail.com>
2014-09-01 13:51:41 +0300

    add checkIsRunning()

--- deploy-many-wars-from-file-on-tomcat6.sh
+++ deploy-many-wars-from-file-on-tomcat6.sh
@@ -13,11 +13,11 @@
 # At the end script prints statistics: download errors, deploy errors and succesfully deployed modules.
 
 # NB! You need to make changes on further rows:
-# 47: username, password, URL and port of your Tomcat manager
-# 61: username, password, URL and port of your Tomcat manager
-# 131: path to catalina logs directory
-# 152: path to webapps directory
-# 154: URL to your war file
+# 157: path to the directory with logs
+# 178: path to webapps directory
+# 180: username, password, URL and port of your Tomcat manager
+# 181: URL to your war file
+# 182: should be the same as downloaded file
 
 NONE='\033[00m'
 RED='\033[01;31m'
@@ -44,7 +44,7 @@ function removeExistingFile() {
 
 function undeployOldVersion() {
   echo -e "\n\tUndeploying old version of $module"
-  undeploy=$(curl "http://username:password@URL:port/manager/undeploy?path=/$module") # username, password, URL and port of your Tomcat manager
+  undeploy=$(curl "$tomcatManager/undeploy?path=/$module")
 
   if echo "$undeploy" | grep -q "OK - Undeployed application at context path"; then
     echo -e "\t${GREEN}OK: old version of $module is undeployed${NONE}"
@@ -58,16 +58,28 @@ function undeployOldVersion() {
 
 function deployNewVersion() {
   echo -e "\n\tDeploying new version $newWar ..."
-  deploy=$(curl --upload-file "$newWar" "http://username:password@URL:port/manager/deploy?path=/$module&update=true") # username, password, URL and port of your Tomcat manager
+  deploy=$(curl --upload-file "$newWar" "$tomcatManager/deploy?path=/$module&update=true")
 
   if echo "$deploy" | grep -q "OK - Deployed application at context path"; then
     echo -e "\t${GREEN}OK: $newWar is deployed${NONE}"
-    successModules+=($line)
     removeDownloadedFile;
   else
     echo $deploy
     echo -e "\t${RED}ERROR: can't deploy $newWar. See logs for details${NONE}"
     deployErrors+=($line)
+  fi
+}
+
+function checkIsRunning() {
+  echo -e "\n\tChecking is $newWar running..."
+  isRunning=$(curl "$tomcatManager/list")
+
+  if echo "$isRunning" | grep -q "$moduleName:running"; then
+    echo -e "\t${GREEN}OK: $newWar is running${NONE}"
+    successModules+=($line)
+  else
+    echo -e "\t${RED}ERROR: $newWar is not running${NONE}"
+    runningErrors+=($line)
   fi
 }
 
@@ -108,6 +120,19 @@ function printDeployErrors() {
   fi
 }
 
+function printRunningErrors() {
+  if [ ${#runningErrors[*]} -gt 0 ]
+  then
+    echo -e "\t\tRUNNING ERRORS: ${RED}${#runningErrors[*]}${NONE}"
+    for item in ${runningErrors[*]}
+    do
+      echo -e "\t\t\t${RED}$item${NONE}"
+    done
+  else
+    echo -e "\t\tRUNNING ERRORS: ${#runningErrors[*]}"
+  fi
+}
+
 function printDeployedModules() {
   if [ ${#successModules[*]} -gt 0 ]
   then
@@ -126,9 +151,10 @@ function printStatistics() {
 
   printDownloadErrors;
   printDeployErrors;
+  printRunningErrors;
   printDeployedModules;
 
-  echo -e "\n\t\tSee logs for details: apache-tomcat/logs/catalina.out" # path to catalina logs directory
+  echo -e "\n\t\tSee logs for details: tomcat/logs/" # TODO: path to the directory with logs
   echo -e "\t\t${GRAY}**************************************************${NONE}"
   echo -e "\n\n"
 }
@@ -149,10 +175,11 @@ file=$1
 while read -r line; do
 
   # Set variables
-  webapps="apache-tomcat/webapps" # path to webapps directory
+  webapps="apache-tomcat/webapps" # TODO: path to webapps directory
   module=$( echo "$line" |cut -d "-" -f1 ) # One line example "module-1.1.1.1" where first word before "-" is module name
-  location="URL/$line.war" # URL to your war file
-  newWar=$line.war # should be the same as downloaded file
+  tomcatManager="http://user:password@URL:port/manager" # TODO: username, password, URL and port of your Tomcat manager
+  location="URL/$line.war" # TODO: URL to your war file
+  newWar=$line.war # TOOD: should be the same as downloaded file
 
   echo -e "\n${CYAN}********************Updating $line********************${NONE}"
 
@@ -169,6 +196,7 @@ while read -r line; do
       echo -e "\n\t${YELLOW}WARNING: can't find previous deployed version of $module${NONE}"
 
       deployNewVersion;
+	  checkIsRunning;
 
       echo -e "${CYAN}********************Update of $line is completed********************${NONE}"
 
@@ -179,6 +207,7 @@ while read -r line; do
     if [[ $isUndeployed -eq 1 ]]
     then
       deployNewVersion;
+	  checkIsRunning;
 
       echo -e "${CYAN}********************Update of $line is completed********************${NONE}"
     fi

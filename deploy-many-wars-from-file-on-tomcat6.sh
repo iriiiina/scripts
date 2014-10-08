394b09e0f5778313d65a73f3e587650c4eed6092
Irina <iriiiina@gmail.com>
2014-10-08 17:14:25 +0300

    Functions in outer file

--- deploy-many-wars-from-file-on-tomcat6.sh
+++ deploy-many-wars-from-file-on-tomcat6.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# author: Irina Ivanova, iriiiina@gmail.com, 31.08.2014
+# author: Irina Ivanova, iriiiina@gmail.com, 8.10.2014
 # Script works with Tomcat 6
 # Script takes module and version number from a file, downloads the war file, undeploys old version, deploys new version and continue with next module and version in the file.
 # Example of rows in the file:
@@ -13,163 +13,26 @@
 # At the end script prints statistics: download errors, deploy errors and succesfully deployed modules.
 
 # NB! You need to make changes on further rows:
-# 157: path to the directory with logs
-# 178: path to webapps directory
-# 180: username, password, URL and port of your Tomcat manager
-# 181: URL to your war file
-# 182: should be the same as downloaded file
+# 41: path to webapps directory
+# 43: username, password, URL and port of your Tomcat manager
+# 44: URL to your war file
 
-NONE='\033[00m'
-RED='\033[01;31m'
-GREEN='\033[01;32m'
-YELLOW='\033[01;33m'
-CYAN='\033[01;36m'
-GRAY='\e[100m'
+. functions-for-deploying-on-tomcat6.sh
 
-function removeExistingFile() {
-  if test -e "$newWar"; then
-    echo -e "\n\t${YELLOW}$newWar file already exists${NONE}"
-    echo -e "\tRemoving existing $newWar file..."
+lock="SOMEBODY_IS_UPDATING_BUNCH_OF_MODULES.loc"
 
-    rm $newWar
-
-    if ! test -e "$newWar"; then
-      echo -e "\t${GREEN}OK: existing file is removed${NONE}"
-    else
-      echo -e "\t${RED}ERROR: can't remove existing file${NONE}"
-      exit
-    fi
-  fi
-}
-
-function undeployOldVersion() {
-  echo -e "\n\tUndeploying old version of $module"
-  undeploy=$(curl "$tomcatManager/undeploy?path=/$module")
-
-  if echo "$undeploy" | grep -q "OK - Undeployed application at context path"; then
-    echo -e "\t${GREEN}OK: old version of $module is undeployed${NONE}"
-    isUndeployed=1
-  else
-    echo $undeploy
-    echo -e "\t${RED}ERROR: can't undeploy old version of $module${NONE}"
-    isUndeployed=0
-  fi
-}
-
-function deployNewVersion() {
-  echo -e "\n\tDeploying new version $newWar ..."
-  deploy=$(curl --upload-file "$newWar" "$tomcatManager/deploy?path=/$module&update=true")
-
-  if echo "$deploy" | grep -q "OK - Deployed application at context path"; then
-    echo -e "\t${GREEN}OK: $newWar is deployed${NONE}"
-    removeDownloadedFile;
-  else
-    echo $deploy
-    echo -e "\t${RED}ERROR: can't deploy $newWar. See logs for details${NONE}"
-    deployErrors+=($line)
-  fi
-}
-
-function checkIsRunning() {
-  echo -e "\n\tChecking is $newWar running..."
-  isRunning=$(curl "$tomcatManager/list")
-
-  if echo "$isRunning" | grep -q "$moduleName:running"; then
-    echo -e "\t${GREEN}OK: $newWar is running${NONE}"
-    successModules+=($line)
-  else
-    echo -e "\t${RED}ERROR: $newWar is not running${NONE}"
-    runningErrors+=($line)
-  fi
-}
-
-function removeDownloadedFile() {
-  echo -e "\n\tRemoving downloaded file..."
-  rm $newWar
-
-  if ! test -e "$newWar"; then
-    echo -e "\t${GREEN}OK: downloaded file is removed${NONE}"
-  else
-    echo -e "\t${RED}ERROR: can't remove file $newWar${NONE}"
-  fi
-}
-
-function printDownloadErrors() {
-  if [ ${#downloadErrors[*]} -gt 0 ]
-  then
-    echo -e "\t\tDOWNLOAD ERRORS: ${RED}${#downloadErrors[*]}${NONE}"
-    for item in ${downloadErrors[*]}
-    do
-      echo -e "\t\t\t${RED}$item${NONE}"
-    done
-  else
-    echo -e "\t\tDOWNLOAD ERRORS: ${#downloadErrors[*]}"
-  fi
-}
-
-function printDeployErrors() {
-  if [ ${#deployErrors[*]} -gt 0 ]
-  then
-    echo -e "\t\tDEPLOY ERRORS: ${RED}${#deployErrors[*]}${NONE}"
-    for item in ${deployErrors[*]}
-    do
-      echo -e "\t\t\t${RED}$item${NONE}"
-    done
-  else
-    echo -e "\t\tDEPLOY ERRORS: ${#deployErrors[*]}"
-  fi
-}
-
-function printRunningErrors() {
-  if [ ${#runningErrors[*]} -gt 0 ]
-  then
-    echo -e "\t\tRUNNING ERRORS: ${RED}${#runningErrors[*]}${NONE}"
-    for item in ${runningErrors[*]}
-    do
-      echo -e "\t\t\t${RED}$item${NONE}"
-    done
-  else
-    echo -e "\t\tRUNNING ERRORS: ${#runningErrors[*]}"
-  fi
-}
-
-function printDeployedModules() {
-  if [ ${#successModules[*]} -gt 0 ]
-  then
-    echo -e "\n\t\tDEPLOYED MODULES: ${GREEN}${#successModules[*]}${NONE}"
-    for item in ${successModules[*]}
-    do
-      echo -e "\t\t\t${GREEN}$item${NONE}"
-    done
-  else
-    echo -e "\n\t\tDEPLOYED MODULES: ${#successModules[*]}"
-  fi
-}
-
-function printStatistics() {
-  echo -e "\n\n\t\t${GRAY}********************STATISTICS********************${NONE}"
-
-  printDownloadErrors;
-  printDeployErrors;
-  printRunningErrors;
-  printDeployedModules;
-
-  echo -e "\n\t\tSee logs for details: tomcat/logs/" # TODO: path to the directory with logs
-  echo -e "\t\t${GRAY}**************************************************${NONE}"
-  echo -e "\n\n"
-}
-
-# Verify arguments: $1 = file with modules and versions. File should contain one module and version per line: module-1.1.1.1 
-if [ $# -ne 1 ]
-then
-  echo -e "\n\t ${RED}Usage: $0 FILE_NAME${NONE}"
-  echo -e "\t Example: $0 modules.txt\n"
+if test -e "SOMEBODY_IS_UPDATING"*; then
+  printError "\n\tERROR: Somebody is updating, see .loc file for details"
+  printError "\n\n"
   exit 0
+else
+  bunchVerifyArguments $#
+  touch $lock
 fi
 
-echo -e "\n\t\t${GRAY}*********************************************${NONE}"
-echo -e "\t\t${GRAY}********************START********************${NONE}"
-echo -e "\t\t${GRAY}*********************************************${NONE}"
+printGray "\n\t\t*********************************************"
+printGray "\t\t********************START********************"
+printGray "\t\t*********************************************"
 
 file=$1
 while read -r line; do
@@ -179,26 +42,26 @@ while read -r line; do
   module=$( echo "$line" |cut -d "-" -f1 ) # One line example "module-1.1.1.1" where first word before "-" is module name
   tomcatManager="http://user:password@URL:port/manager" # TODO: username, password, URL and port of your Tomcat manager
   location="URL/$line.war" # TODO: URL to your war file
-  newWar=$line.war # TOOD: should be the same as downloaded file
+  newWar=$line.war # should be the same as downloaded file
 
-  echo -e "\n${CYAN}********************Updating $line********************${NONE}"
+  printInfo "\n********************Updating $line********************"
 
   removeExistingFile;
 
   # Download file
-  echo -e "\n\tDownloading $line file"
+  printInfo "\n\tDownloading $line file"
   wget $location
 
   if test -e $newWar; then
-    echo -e "\t${GREEN}OK: file $newWar is downloaded${NONE}"
+    printOk "\tOK: file $newWar is downloaded"
 
     if ! test -d "$webapps/$module" && ! test -e "$webapps/$module.war"; then
-      echo -e "\n\t${YELLOW}WARNING: can't find previous deployed version of $module${NONE}"
+      printWarning "\n\tWARNING: can't find previous deployed version of $module"
 
       deployNewVersion;
 	  checkIsRunning;
 
-      echo -e "${CYAN}********************Update of $line is completed********************${NONE}"
+      printInfo "********************Update of $line is completed********************"
 
     else
       undeployOldVersion;
@@ -209,14 +72,18 @@ while read -r line; do
       deployNewVersion;
 	  checkIsRunning;
 
-      echo -e "${CYAN}********************Update of $line is completed********************${NONE}"
+      printInfo "********************Update of $line is completed********************"
     fi
   else
-    echo -e "\t ${RED}ERROR: can't download the $line.war file from $location${NONE}"
+    printError "\tERROR: can't download the $line.war file from $location"
     downloadErrors+=($line)
-    echo -e "${CYAN}********************Update of $line is completed********************${NONE}"
+    printInfo "********************Update of $line is completed********************"
   fi
 
 done < $file
 
 printStatistics;
+
+if test -e "$lock"; then
+  rm $lock
+fi
--- deploy-new-war-on-tomcat6.sh
+++ deploy-new-war-on-tomcat6.sh
@@ -1,127 +1,47 @@
 #!/bin/bash
 
-# author: Irina Ivanova, iriiiina@gmail.com, 31.08.2014
+# author: Irina Ivanova, iriiiina@gmail.com, 8.10.2014
 # Script works with Tomcat 6
 # Script downloads war file, undeploys old version of module and deploys downloaded version
 
 # NB! You need to make changes on further rows:
-# 87: path to the directory with logs
-# 111: path to your webapps directory
-# 112: username, password, URL and port of your Tomcat manager
-# 113: URL to your war file
+# 26: path to your webapps directory
+# 27: username, password, URL and port of your Tomcat manager
+# 28: URL to your war file
 
-NONE='\033[00m'
-RED='\033[01;31m'
-GREEN='\033[01;32m'
-YELLOW='\033[01;33m'
-CYAN='\033[01;36m'
+. functions-for-deploying-on-tomcat6.sh
 
-function removeExistingFile() {
-  if test -e "$newWar"; then
-    echo -e "\n\t${YELLOW}$newWar file already exists${NONE}"
-    echo -e "\t${CYAN}Removing existing $newWar file...${NONE}"
+lock="SOMEBODY_IS_UPDATING_$1-$2.loc"
 
-    rm $newWar
-
-    if ! test -e "$newWar"; then
-      echo -e "\t${GREEN}OK: existing file is removed${NONE}"
-    else
-      echo -e "\t${RED}ERROR: can't remove existing file${NONE}"
-      exit
-    fi
-  fi
-}
-
-function downloadFile() {
-  echo -e "\n\t${CYAN}Downloading file $newWar...${NONE}"
-  wget $location
-  exitCode=$?
-  if [ $exitCode -ne 0 ]
-  then
-    echo -e "\t${RED}ERROR: can't download file from $location${NONE}"
-    exit 1
-  fi
-
-  if test -e "$newWar"; then
-    echo -e "\t${GREEN}OK: file $newWar is downloaded${NONE}"
-  fi
-}
-
-function undeployOldVersion() {
-  echo -e "\n\t${CYAN}Undeploying old version of $module...${NONE}"
-  undeploy=$(curl "$tomcatManager/undeploy?path=/$module")
-
-  if echo "$undeploy" | grep -q "OK - Undeployed application at context path"; then
-    echo -e "\t${GREEN}OK: old version of $module is undeployed${NONE}"
-    isUndeployed=1
-  else
-    echo $undeploy
-    echo -e "\t${RED}ERROR: can't undeploy old version of $module${NONE}"
-    isUndeployed=0
-  fi
-}
-
-function deployNewVersion() {
-  echo -e "\n\t${CYAN}Deploying new version $newWar...${NONE}"
-  deploy=$(curl --upload-file "$newWar" "$tomcatManager/deploy?path=/$module&update=true")
-
-  if echo "$deploy" | grep -q "OK - Deployed application at context path"; then
-    echo -e "\t${GREEN}OK: $newWar is deployed${NONE}"
-
-    removeDownloadedFile;
-  else
-    echo $deploy
-    echo -e "\t${RED}ERROR: can't deploy $newWar. See logs for details${NONE}"
-  fi
-}
-
-function checkIsRunning() {
-  echo -e "\n\t${CYAN}Checking is $newWar running...${NONE}"
-
-  isRunning=$(curl "$tomcatManager/list")
-
-  if echo "$isRunning" | grep -q "$module:running"; then
-    echo -e "\t${GREEN}OK: $newWar is running${NONE}"
-  else
-    echo -e "\t${RED}ERROR: $newWar can't run${NONE}"
-	echo -e "\t${RED}See logs: tomcat/logs/${NONE}" # path to the directory with logs
-  fi
-}
-
-function removeDownloadedFile() {
-  echo -e "\n\t${CYAN}Removing downloaded file...${NONE}"
-  rm $newWar
-
-  if ! test -e "$newWar"; then
-    echo -e "\t${GREEN}OK: downloaded file is removed${NONE}"
-  else
-    echo -e "\t${RED}ERROR: can't remove file $newWar${NONE}"
-  fi
-}
-
-# Verify arguments: first argument $1 is module name, second $2 is version number
-if [ $# -ne 2 ]
-then
-  echo -e "\n\t${RED}Usage: $0 MODULE_NAME MODULE_VERSION${NONE}"
-  echo -e "\tExample: $0 admin 1.1.1.1\n"
+if test -e "SOMEBODY_IS_UPDATING"*; then
+  printError "\n\tERROR: Somebody is updating, see .loc file for details"
+  printError "\n\n"
   exit 0
+else
+  verifyArguments $#
+  touch $lock
 fi
 
 # Set variables
-webapps="tomcat/webapps" # path to your webapps directory
-tomcatManager="http://username:password@URL:port/manager" # username, password, URL and port of your Tomcat manager
-location="URL/$1-$2.war" # URL to your war file
+webapps="tomcat/webapps" # TODO: path to your webapps directory
+tomcatManager="http://username:password@URL:port/manager" # TODO: username, password, URL and port of your Tomcat manager
+location="URL/$1-$2.war" # TODO: URL to your war file
 newWar="$1-$2.war" # should be the same as downloaded file
 module=$1
 
-echo -e "\n\t${CYAN}**********$module**********${NONE}"
+printInfo "\n\t**********$module**********"
 
 removeExistingFile;
 
 downloadFile;
 
+if ! test -e "$newWar"; then
+  rm $lock
+  exit
+fi
+
 if ! test -d "$webapps/$module" && ! test -e "$webapps/$module.war"; then
-  echo -e "\n\t${YELLOW}WARNING: can't find previous deployed version of $module${NONE}"
+  printWarning "\n\tWARNING: can't find previous deployed version of $module"
   deployNewVersion;
   checkIsRunning;
   exit
@@ -133,4 +53,8 @@ if [[ $isUndeployed -eq 1 ]]
 then
   deployNewVersion;
   checkIsRunning;
+fi
+
+if test -e "$lock"; then
+  rm $lock
 fi

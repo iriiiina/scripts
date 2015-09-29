3696c7cf18079ec89a2f521456c3d3df2c01d336
Irina Ivanova <irina@irina.webmedia.int>
2015-09-29 11:56:50 +0300

    Modify find-specification-by-name.sh

--- find-specification-by-name.sh
+++ find-specification-by-name.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 # Author: Irina.Ivanova@nortal.com, 28.09.2015
-# v0.1
+# v1.0
 
 path="/Users/irina/Desktop/specs"
+text=$*
+lower=$(echo $text | tr '[:upper:]' '[:lower:]')
+upper=$(echo $text | tr '[:lower:]' '[:upper:]')
 
-find $path -iname *$1*
+NONE='\e[0m'
+RED='\e[31m'
+
+function printError() {
+  printf "${RED}$1${NONE}\n"
+}
+
+function notificate() {
+  printf '\a'
+}
+
+function verifyArguments() {
+  if [ $1 -lt 1 ]; then
+    printError "Usage: $0 ARGUMENT_1 ARGUMENT_2 ...";
+    notificate;
+    exit
+  fi
+}
+
+function findFile() {
+  find $path -iname "*$text*" | awk -v text="$text" -v lower="$lower" -v upper="$upper" '{ gsub(text, "\033[36m&\033[0m"); gsub(lower, "\033[36m&\033[0m"); gsub(upper, "\033[36m&\033[0m"); print }'
+}
+
+verifyArguments $#;
+findFile;
+
+notificate;

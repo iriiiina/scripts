7de375e4c353397cfa73140141486304ab2edcdf
Irina Ivanova <irina@irina.webmedia.int>
2015-09-29 12:31:33 +0300

    Modify open-last-specification.sh

--- open-last-specification.sh
+++ open-last-specification.sh
@@ -1,9 +1,68 @@
 #!/bin/bash
 # Author: Irina.Ivanova@nortal.com, 28.09.2015
-# v0.1
+# v1.0
 
 path="/Users/irina/Desktop/specs"
 
-file=$( find $path -iname *$1* | sort -n | tail -1 )
+text=$*
 
-open "$file"
+NONE='\e[0m'
+RED='\e[31m'
+CYAN='\e[36m'
+YELLOW='\e[33m'
+
+function printError() {
+  printf "${RED}$1${NONE}\n"
+}
+
+function printInfo() {
+  printf "${CYAN}$1${NONE}\n"
+}
+
+function printWarning() {
+  printf "${YELLOW}$1${NONE}\n"
+}
+
+function notificate() {
+  printf '\a'
+}
+
+function verifyArguments() {
+  if [ $1 -lt 1 ]; then
+    printError "Usage: $0 ARGUMENT_1 ARGUMENT_2 ...";
+    notificate;
+    exit
+  fi
+}
+
+function findFile() {
+  file=$( find $path -iname "*$text*" | sort -n | tail -1 )
+}
+
+function checkIsFileIsDoc() {
+  if [[ $file == *'.doc' ]]; then
+    doc=1
+  else
+    doc=0
+  fi
+}
+
+function openFile() {
+  printInfo "Opening $file...";
+  open "$file"
+}
+
+verifyArguments $#;
+findFile;
+checkIsFileIsDoc;
+
+if [[ $doc == 1 ]]; then
+  openFile;
+else
+  printWarning "WARNING: Found file $file is not .DOC. Do you still want to open it? y/n";
+  read open
+
+  if [[ $open == 'y' || $open == 'Y' || $open == 'yes' || $open == 'YES' ]]; then
+    openFile;
+  fi
+fi

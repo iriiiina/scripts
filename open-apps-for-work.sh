4dd1dcbddf75a202354106be4607b2fad70f3cd4
Irina Ivanova <iriiiina@gmail.com>
2016-01-29 11:10:27 +0200

    Add open-apps-for-work.sh

---
+++ open-apps-for-work.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Open all applications needed for work
+# Irina Ivanova, iriiiina@gmail.com
+# v0.1, 29.01.2016
+
+path="/Applications"
+
+open "$path/Calendar.app"
+open "$path/Fleep.app"
+open "$path/Mail.app"
+open "$path/Notes.app"
+open "$path/Reminders.app"
+open "$path/Safari.app"
+open "$path/Skype.app"

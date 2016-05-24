917818e9e908f455d3a39e13b4ae607d48ae8e5e
Irina Ivanova <iriiiina@gmail.com>
2016-05-24 11:09:27 +0300

    Add rename-log-files.sh

---
+++ rename-log-files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+### Rename log files: module.log.01.12.2015 -> module.log.2015.12.01
+### Script should be located in the same directory as log files, than need to be renamed.
+### It was used to change format of the date from dd.MM.yyyy to yyy.MM.dd to fix file sorting by name.
+###
+### Author: Irina Ivanova, iriiiina@gmail.com, 24.05.2016
+
+for f in *.log.*; do
+  module=$( echo "$f" | cut -d "." -f1 )
+  day=$( echo "$f" | cut -d "." -f3 )
+  month=$( echo "$f" | cut -d "." -f4 )
+  year=$( echo "$f" | cut -d "." -f5 )
+
+  mv -n "$f" "$module.log.$year.$month.$day"
+done
